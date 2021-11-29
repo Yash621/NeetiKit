27c7af4a6cc1826de2ddcb164ca9cec9a0925283
yash621 <yg17381@gmail.com>
2021-11-29 22:26:39 +0530

    updated

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
-    ordering = ['-date_posted']
+    ordering = ['date_posted']
 
 
 def about(request):
