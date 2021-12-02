c67a9c04c040a3312add5237bb11dbde9090a027
yash621 <yg17381@gmail.com>
2021-12-02 22:09:40 +0530

    added delete view

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('about', views.about, name="blog-about"),
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('post/new/', PostCreateView.as_view(), name="post-create"),
-    path('post/<int:pk>/', PostUpdateView.as_view(), name="post-update"),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name="post-update"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, updateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 def home(request):
@@ -32,13 +32,29 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class PostUpdateView(LoginRequiredMixin, updateView):
+class PostUpdateView(LoginRequiredMixin, updateView, UserPassesTestMixin):
     model = Post
     fields = ['title', 'current']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView, UserPassesTestMixin):
+    model = Post
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
