fad704c5312129107638531e905c41a137cfb82e
yash621 <yg17381@gmail.com>
2021-12-05 21:29:29 +0530

    added password reset template

--- blogger_media/urls.py
+++ blogger_media/urls.py
@@ -25,7 +25,11 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
     path("blog/", include("blog.urls")),
-    path('profile/', user_views.profile, name="profile")
+    path('profile/', user_views.profile, name="profile"),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name="users/password_reset_done.html"), name="password_reset"),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name="users/password_reset.html"), name="password_reset"),
 ]
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL,
