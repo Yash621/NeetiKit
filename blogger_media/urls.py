ce1cbf895e65eca66d090c27902633ba76d589b8
yash621 <yg17381@gmail.com>
2021-12-05 21:43:21 +0530

    added url

--- blogger_media/urls.py
+++ blogger_media/urls.py
@@ -30,6 +30,8 @@ urlpatterns = [
         template_name="users/password_reset_done.html"), name="password_reset"),
     path('password-reset/', auth_views.PasswordResetView.as_view(
         template_name="users/password_reset.html"), name="password_reset"),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name="users/password_reset_confirm.html"), name="password_reset_confirm"),
 ]
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL,
